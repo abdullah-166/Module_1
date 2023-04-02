3304c86ba90c80b66adf04079fdd5d7c16565e8b
unknown <22103063@iubat.edu>
2023-04-02 21:11:57 +0600

    Module_1

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    printf("Hello World!!!\n");
+    printf("I am Abdullah Al Arif\n\t");
+    printf("Now we are passing a very good time in Ramadan.");
+    return 0;
+}
---
+++ user_input.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter the value of a and b: ");
+    scanf("%d %d",&a,&b);
+    printf("\nSum of A and B is: %d", a+b);
+
+    return 0;
+}
